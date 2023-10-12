85f208d63c3d32b8176f50d996e24f6ed35ea8f8
zacharytaylor-FS <ZJTaylor@student.fullsail.edu>
2023-10-12 04:50:08 -0400

    Update Home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,9 @@
 import Image from 'next/image'
 
-export default function Home() {
+export default function HomePage() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-10">
-      <header className='flex justify-space w-full'>
+      {/* <header className='flex justify-space w-full'>
         <div className='flex-1'>
           <a className='hover:underline' href="/">
             <span>Logo</span>
@@ -14,7 +14,7 @@ export default function Home() {
           <div className='mr-6'><a href='about'>About</a></div>
           <div className='mr-6'><a href='contact'>Contact</a></div>
         </nav>
-      </header>
+      </header> */}
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Get started by editing&nbsp;
@@ -41,14 +41,15 @@ export default function Home() {
       </div>
 
       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
+        <h1 className='text-5xl font-medium'>Home Page</h1>
+        {/* <Image
           className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
           src="/next.svg"
           alt="Next.js Logo"
           width={180}
           height={37}
           priority
-        />
+        /> */}
       </div>
 
       {/* <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
